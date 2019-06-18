c13e79d51901de71478f990cef97fb1ff4c4f2f2
Sarah Weatherbee <sarahmweatherbee@gmail.com>
2019-06-17 19:22:50 -0500

    importing goats data

--- src/App/App.js
+++ src/App/App.js
@@ -1,28 +1,27 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
+import goats from './goats';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button className='btn btn-danger'>Goats</button>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // axios calls to get data - anything that modifies state
+  state ={
+    goats: [],
+  }
+
+  componentDidMount() {
+    this.setState({ goats });
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <div>Goat Yoga</div>
+      {/* <GoatCorral goats={goats}/> */}
+      </div>
+    );
+  }
 }
+
 
 export default App;
